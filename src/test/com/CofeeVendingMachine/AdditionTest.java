0fbfb1cc09296f412c85fabb0b065b72fa7ae7ac
Joris Rietveld <jorisrietveld@gmail.com>
2019-01-10 12:46:48 +0100

    Added a test class.

---
+++ src/test/com/CofeeVendingMachine/AdditionTest.java
@@ -0,0 +1,40 @@
+package com.CofeeVendingMachine;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdditionTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @Test
+    void getPrice()
+    {
+    }
+
+    @Test
+    void setPrice()
+    {
+    }
+
+    @Test
+    void getName()
+    {
+    }
+
+    @Test
+    void setName()
+    {
+    }
+
+    @Test
+    void getInvetoryCost()
+    {
+    }
+}
