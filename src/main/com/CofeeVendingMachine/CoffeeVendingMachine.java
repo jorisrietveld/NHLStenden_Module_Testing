5b272efa5cd6d0f98fbca0bb22c5f8016c642179
Joris Rietveld <jorisrietveld@gmail.com>
2019-01-18 14:10:06 +0100

    Added Initial inventory.

--- src/main/com/CofeeVendingMachine/CoffeeVendingMachine.java
+++ src/main/com/CofeeVendingMachine/CoffeeVendingMachine.java
@@ -3,7 +3,9 @@ package com.CofeeVendingMachine;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -13,13 +15,25 @@ public class CoffeeVendingMachine
 
     private Inventory inventory;
 
+    private static Map<Product, Integer> getInitialInventory()
+    {
+        Map<Product, Integer> initialInventory = new LinkedHashMap<>();
+        initialInventory.put( new Beverage("Coffee", BigDecimal.ZERO ), 100 );
+        initialInventory.put( new Beverage("ColdWater", new BigDecimal("12.00") ), 100 );
+        initialInventory.put( new Beverage("HotWater", new BigDecimal("6.00") ), 100 );
+        initialInventory.put( new Beverage("ChocolateMilk", BigDecimal.ONE ), 100 );
+
+        return initialInventory;
+    }
+
     /**
      * The access point to this program.
      * @param args THe commandline arguments passed to this program.
      */
     public static void main( String[] args ) throws Exception
     {
-        CoffeeVendingMachine coffeeVendingMachine = new CoffeeVendingMachine();
+
+        CoffeeVendingMachine coffeeVendingMachine = new CoffeeVendingMachine( getInitialInventory() );
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter String");
@@ -33,7 +47,6 @@ public class CoffeeVendingMachine
         }catch(NumberFormatException nfe){
             System.err.println("Invalid Format!");
         }
-        gsdgsdfgfgh;
     }
 
 
@@ -51,7 +64,7 @@ public class CoffeeVendingMachine
      * Create a new coffee vending machine instance with some stock in inventory.
      * @param initialInventory The initial stock in inventory.
      */
-    public CoffeeVendingMachine( Map<Product, Integer> initialInventory)
+    public CoffeeVendingMachine( Map<Product, > initialInventory)
     {
         this.inventory = new Inventory( initialInventory );
         this.orderedProducts = new HashSet<>( );
