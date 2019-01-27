531b05492b7561d11c5c24dc74e2af6aa96fac51
Joris Rietveld <jorisrietveld@gmail.com>
2019-01-27 16:31:10 +0100

    Writing the boot, reboot and shutdown procedures for the coffee vending machine.

--- src/main/com/CofeeVendingMachine/CoffeeVendingMachine.java
+++ src/main/com/CofeeVendingMachine/CoffeeVendingMachine.java
@@ -2,6 +2,7 @@ package com.CofeeVendingMachine;
 
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.util.*;
@@ -13,14 +14,14 @@ public class CoffeeVendingMachine
     /**
      * Constants that define the terminal output formats.
      */
-    final static private String MENU_OPT_FORMAT =  "-----------[%1$22s]----------";
-    final static private String MENU_HEAD_FORMAT = "[%d] - %s";
+    final static private String MENU_HEAD_FORMAT = "-----------[%1$22s]----------";
+    final static private String MENU_OPT_FORMAT = "[%d] - %s";
     final static private String MENU_FOOT_FORMAT = "---------------------------------------------";
 
     /**
      * The buffer for reading users input.
      */
-    private BufferedReader bufferedReader = new BufferedReader(new InputStreamReader( in));
+    private BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( in ) );
 
     /**
      * All products that are ordered by customers.
@@ -33,62 +34,37 @@ public class CoffeeVendingMachine
     private Inventory inventory;
 
     /**
+     * If set false it will initiate a shutdown.
+     */
+    private boolean exitSignal = false;
+
+    /**
      * An initiation method that configures the coffee vending with the default
      * stock and price configuration.
+     *
      * @return The initial stock.
      */
     private static Map<Orderable, Integer> getInitialInventory()
     {
         Map<Orderable, Integer> initialInventory = new LinkedHashMap<>();
-        initialInventory.put( new Beverage("Coffee", BigDecimal.ZERO ), 100 );
-        initialInventory.put( new Beverage("ColdWater", new BigDecimal("12.00") ), 100 );
-        initialInventory.put( new Beverage("HotWater", new BigDecimal("6.00") ), 100 );
-        initialInventory.put( new Beverage("ChocolateMilk", BigDecimal.ONE ), 100 );
+        initialInventory.put( new Beverage( "Coffee", BigDecimal.ZERO ), 100 );
+        initialInventory.put( new Beverage( "ColdWater", new BigDecimal( "12.00" ) ), 100 );
+        initialInventory.put( new Beverage( "HotWater", new BigDecimal( "6.00" ) ), 100 );
+        initialInventory.put( new Beverage( "ChocolateMilk", BigDecimal.ONE ), 100 );
 
         return initialInventory;
     }
 
     /**
      * The access point to this program.
+     *
      * @param args THe commandline arguments passed to this program.
      */
-    public static void main( String[] args ) throws Exception
+    public static void main( String ... args )
     {
         CoffeeVendingMachine coffeeVendingMachine = new CoffeeVendingMachine( getInitialInventory() );
-
-
-        out.println("-----------[Coffee vending machine]----------");
-
-
-        for ( Beverage beverage : coffeeVendingMachine.inventory.getBeverges() )
-        {
-            out.format( "[] - make a %s", beverage );
-        }
-
-        String string = bufferedReader.readLine();
-
-        out.print("Enter Integer:");
-
-        try{
-            int i = Integer.parseInt(bufferedReader.readLine());
-        }catch(NumberFormatException nfe){
-            err.println("Invalid Format!");
-        }
+        coffeeVendingMachine.boot();
     }
-
-    public void printMainMenu()
-    {
-        // Todo write out.format( this.MENU_FORMAT) items with the available beverages.
-
-        // Todo write reader that checks for the menu options and calls the order beverage method
-        // Todo     or for the m character for maintenance.
-
-        // todo catch exceptions and print them.
-
-        // todo write exit function
-    }
-
-    // todo write method that can order a beverage.
 
     /**
      * Create a new coffee vending machine instance.
@@ -101,16 +77,93 @@ public class CoffeeVendingMachine
 
     /**
      * Create a new coffee vending machine instance with some stock in inventory.
+     *
      * @param initialInventory The initial stock in inventory.
      */
     public CoffeeVendingMachine( Map<Orderable, Integer> initialInventory )
     {
         this.inventory = new Inventory( initialInventory );
-        this.orderedProducts = new HashSet<>( );
+        this.orderedProducts = new HashSet<>();
     }
 
     /**
-     *
+     * This function boots the system and handles the main program loop.
+     */
+    public void boot()
+    {
+        try
+        {
+            while ( !this.exitSignal )
+            {
+                this.printMainMenu();
+            }
+            // Todo call a terminate function.
+        }
+        catch ( Exception ex )
+        {
+            out.format( MENU_HEAD_FORMAT, "An exception was thrown" );
+            ex.printStackTrace();
+        }
+    }
+
+    public void reboot()
+    {
+        // todo write message to notify the user about the reboot.
+        // todo re-initiate the ordered products and inventory to its defaults
+        // todo      and call the boot function again.
+    }
+
+    /**
+     * This will do a complete shutdown the coffee vending machine, it terminate
+     * its self and returnes to the executing terminal.
+     */
+    public void shutdown()
+    {
+        //
+    }
+
+    public void printMainMenu() throws IOException
+    {
+        // Print the header of the main menu
+        out.format( MENU_HEAD_FORMAT, "Coffee Vending Menu" );
+
+        // Look in the inventory for available beverages and print a menu for the user.
+        List<Beverage> beverges = this.inventory.getBeverges();
+        for ( int i = 0, beverageSize = beverges.size(); i < beverageSize; i++ )
+        {
+            out.format( MENU_OPT_FORMAT, i, beverges.get( i ).getName() );
+        }
+
+        // Add an method to exit or enter maintenance mode.
+        out.format( "%1$s%nType m for maintainable mode or q to exit.%n%1$s", MENU_FOOT_FORMAT );
+
+        String answer = bufferedReader.readLine();
+
+        // The user choose to quit the program so set the exit signal.
+        if ( answer.contains( "q" ) )
+        {
+            this.exitSignal = true;
+            return;
+        }
+
+        // The user wants to access maintainer mode.
+        if ( answer.contains( "m" ) )
+        {
+            // todo write maintainer mode.
+        }
+
+        try
+        {
+            Integer chosenMenuIndex = Integer.parseInt( answer );
+            // todo Check if the index exists.
+            // todo call make beverage with the name of the beverage to make
+        }
+        catch ( NumberFormatException ex )
+        {
+
+        }
+    }
+    /**
      * @param product
      */
     public void orderProduct( Orderable product )
